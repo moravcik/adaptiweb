4e9399b59a5862b93e508efa8a45d93a03083d2b
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-05-07 05:22:50 +0000

    Servlet for loading GWT resources.

---
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/servlet/GwtResourceFilter.java
@@ -0,0 +1,83 @@
+package com.adaptiweb.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Proxy for GWT resources. 
+ */
+public class GwtResourceFilter implements Filter {
+	
+	private static final String GWT_MODULE = "gwtModule";
+
+	//TODO make configurable url pattern matching
+	//TODO better is ant-like pattern 
+	private static final Pattern SUPPORTED_EXTENSIONS = Pattern.compile(
+			".*\\.(css|js|html|png|gif)", Pattern.CASE_INSENSITIVE);
+	
+	private ServletContext context;
+
+	private String gwtModule;
+
+	@Override
+	public void destroy() {
+		context = null;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String path = req.getRequestURI().substring(req.getContextPath().length());
+		
+		if(SUPPORTED_EXTENSIONS.matcher(path).matches()) {
+			String resource = "/WEB-INF/" + gwtModule + path;
+			InputStream input = context.getResourceAsStream(resource);
+			
+			if(input == null) chain.doFilter(request, response);
+			else {
+				assert log("gwt-resource: " + path);
+				HttpServletResponse resp = (HttpServletResponse) response;
+				resp.setHeader("Content-Type", context.getMimeType(path));
+				resp.setHeader("Cache-Control", path.contains(".cache.") ?
+						"public, max-age=31536000" : "no-cache");
+				writeResourceStream(input, resp);
+			}
+		}
+		else chain.doFilter(request, response);
+	}
+
+	private boolean log(String msg) {
+		System.out.println(msg);
+		return true;
+	}
+
+	private void writeResourceStream(InputStream input, HttpServletResponse resp) throws IOException {
+		try {
+			IOUtils.copy(input, resp.getOutputStream());
+		} finally {
+			IOUtils.closeQuietly(input);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		context = filterConfig.getServletContext();
+		gwtModule = filterConfig.getInitParameter(GWT_MODULE);
+		if(gwtModule == null) throw new ServletException("Missing init parameter " 
+				+ GWT_MODULE + " for filter " + filterConfig.getFilterName() + "!");
+	}
+
+}
