993284564d5ae2f47960089489df27428e40e335
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-05-21 13:28:27 +0000

    Sharing some tiny projects.

---
+++ projects/aw-utils/trunk/type-analyzer/src/main/java/com/adaptiweb/utils/typeanalyzer/CollectionTypeAnalysis.java
@@ -0,0 +1,70 @@
+package com.adaptiweb.utils.typeanalyzer;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+class CollectionTypeAnalysis implements TypeAnalysis {
+	
+	private final Class<?> raw;
+	private final Type component;
+	private final ParameterizedType type;
+
+	public CollectionTypeAnalysis(ParameterizedType type) {
+		this.type = type;
+		Class<?> raw = (Class<?>) type.getRawType();  
+		if(raw.isInterface()) {
+			if(raw.isAssignableFrom(List.class)) raw = ArrayList.class;
+			else if(raw.isAssignableFrom(Set.class)) raw = HashSet.class;
+			else if(raw.isAssignableFrom(Queue.class)) raw = LinkedList.class;
+			else throw new UnsupportedOperationException();
+		}
+		this.raw = raw;
+		this.component = type.getActualTypeArguments()[0];
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object composite(LinkedList<Object> list) {
+		try {
+			Collection<Object> instance = (Collection<Object>) raw.newInstance();
+			instance.addAll(list);
+			return instance;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public Object createInstance() {
+		try {
+			return raw.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Type getComponent() {
+		return component;
+	}
+
+	public TypeNature getNature() {
+		return TypeNature.MULTIPLE;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public Object parse(String data) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ projects/aw-utils/trunk/xml-bind/src/main/java/com/adaptiweb/utils/xmlbind/BindUtils.java
@@ -0,0 +1,70 @@
+package com.adaptiweb.utils.xmlbind;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class BindUtils {
+
+	private final static Pattern xmlNamePattern = Pattern.compile("-([a-z])");
+
+	public static String toJavaName(String xmlName) {
+		Matcher m = xmlNamePattern.matcher(xmlName);
+		StringBuffer result = new StringBuffer();
+		while(m.find())
+			m.appendReplacement(result, m.group(1).toUpperCase());
+		m.appendTail(result);
+		result.setCharAt(0, Character.toUpperCase(result.charAt(0)));
+		return result.toString();
+	}
+
+	public static String toXmlName(String javaName) {
+		StringBuffer result = new StringBuffer();
+		
+		for(int  i = 0, n = javaName.length(); i < n; i++) {
+			char c = javaName.charAt(i);
+			if(i == 0)
+				result.append(n == 1 || !Character.isUpperCase(javaName.charAt(i + 1)) ? Character.toLowerCase(c) : c);
+			else if(Character.isUpperCase(c) && !Character.isUpperCase(javaName.charAt(i - 1))) 
+				result.append('-').append(Character.toLowerCase(c));
+			else
+				result.append(c);
+		}
+		return result.toString();
+	}
+	
+	public static <T> T unmarshal(InputStream is, String characterSetName, Class<T> targetType) throws IOException {
+		return targetType.cast(XmlParser.parse(new InputStreamReader(is, characterSetName), new UnmarshallerHandler(targetType)));
+	}
+
+	public static <T> T unmarshal(Reader reader, Class<T> targetType) throws IOException {
+		return targetType.cast(XmlParser.parse(reader, new UnmarshallerHandler(targetType)));
+	}
+	
+	public static <T> T unmarshal(String xmlFragment, Class<T> targetType) throws IOException {
+		return targetType.cast(XmlParser.parse(new StringReader(xmlFragment), new UnmarshallerHandler(targetType)));
+	}
+
+	public static String marshall(Object source) {
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			
+			new Marshaller().marshal(source, os);
+			
+			os.flush();
+			os.close();
+			
+			return new String(os.toByteArray(), "UTF-8");
+		} catch (ParserException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
