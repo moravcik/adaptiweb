daf7b64440bfe3564435ddb213dbbaf4b57743c1
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-09-16 13:17:01 +0000

    Modified GwtResourceFilter for backward's compatibility

--- projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/server/servlet/GwtResourceFilter.java
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/server/servlet/GwtResourceFilter.java
@@ -1,40 +1,58 @@
 package com.adaptiweb.server.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Pattern;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
 /**
  * Proxy for GWT resources. 
  */
 public class GwtResourceFilter implements Filter {
 	
-	private ServletContext context;
+	private FilterConfig config;
+	private Pattern includePattern;
+	private Pattern excludePattern;
+	private String pathPrefix;
 
 	@Override
 	public void destroy() {
-		context = null;
+		config = null;
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		String path = req.getRequestURI().substring(req.getContextPath().length());
+		String path = getRequestPath(request);
 
 		assert log("gwt-resource: " + path);
 		HttpServletResponse resp = (HttpServletResponse) response;
-		resp.setHeader("Content-Type", context.getMimeType(path));
+		resp.setHeader("Content-Type", getMimeType(path));
 		resp.setHeader("Cache-Control", path.contains(".cache.") ? "public, max-age=31536000" : "no-cache");
 		
-		chain.doFilter(request, response);
+		InputStream input = openResourceDirectly(path);
+		if(input == null) chain.doFilter(request, response);
+		else writeAndCloseResourceStream(input, resp);
+	}
+
+	protected String getRequestPath(ServletRequest request) {
+		HttpServletRequest req = (HttpServletRequest) request;
+		return req.getRequestURI().substring(req.getContextPath().length());
+	}
+
+	protected InputStream openResourceDirectly(String path) {
+		if (includePattern == null || !includePattern.matcher(path).matches()) return null;
+		if (excludePattern != null && excludePattern.matcher(path).matches()) return null;
+		return config.getServletContext().getResourceAsStream(pathPrefix + path); 
 	}
 
 	private boolean log(String msg) {
@@ -44,7 +62,36 @@ public class GwtResourceFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		context = filterConfig.getServletContext();
+		config = filterConfig;
+		includePattern = asPatern(initParameter("includePattern"));
+		excludePattern = asPatern(initParameter("excludePattern"));
+		pathPrefix = initParameter("pathPrefix", "");
+	}
+
+	protected Pattern asPatern(String expression) {
+		return expression == null ? null : Pattern.compile(expression);
+	}
+
+	protected String initParameter(String name) {
+		return initParameter(name, null);
+	}
+	
+	protected String initParameter(String name, String defaultValue) {
+		String result = config.getInitParameter(name);
+		if (result == null) result = config.getServletContext().getInitParameter(name);
+		return result == null ? defaultValue : result;
+	}
+
+	protected void writeAndCloseResourceStream(InputStream input, HttpServletResponse resp) throws IOException {
+		try {
+			IOUtils.copy(input, resp.getOutputStream());
+		} finally {
+			IOUtils.closeQuietly(input);
+		}
+	}
+
+	protected String getMimeType(String path) {
+		return config.getServletContext().getMimeType(path);
 	}
 
 }
