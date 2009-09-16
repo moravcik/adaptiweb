1b7ab0c0b2ddb622b4e315f7ad2049b35637f059
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-09-16 21:25:59 +0000

    Implemented Advice for ServletContext.getResourceAsSteram method. This advice suggest where to find serialization policy file of GWT module.

---
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/aop/AlternativeWebResourceLocator.java
@@ -0,0 +1,31 @@
+package com.adaptiweb.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class AlternativeWebResourceLocator implements MethodInterceptor {
+
+	private String pahtPrefix;
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		Object result = invocation.proceed();
+		if (result == null && pahtPrefix != null) {
+			alterPath(invocation.getArguments());
+			result = invocation.proceed();
+		}
+		return result;
+	}
+
+	private void alterPath(Object[] arguments) {
+		arguments[0] = pahtPrefix + arguments[0];
+	}
+
+	public void setPahtPrefix(String pahtPrefix) {
+		this.pahtPrefix = pahtPrefix;
+	}
+
+	public String getPahtPrefix() {
+		return pahtPrefix;
+	}
+}
