cbde9bef180f40d6ec749f752737fea33fe20fc5
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-10-27 22:19:06 +0000

    SpringUtils shouldn't have so specialized method like parseProxy. (I move it in to ProxyUtils in solargis project)

--- projects/aw-utils/trunk/commons-utils/src/main/java/com/adaptiweb/utils/commons/StringUtils.java
+++ projects/aw-utils/trunk/commons-utils/src/main/java/com/adaptiweb/utils/commons/StringUtils.java
@@ -1,30 +1,12 @@
 package com.adaptiweb.utils.commons;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public final class StringUtils {
 	
 	private StringUtils() {}
 	
-	private static final Pattern PROXY_DEF_PATTERN = Pattern.compile("([^\\:]+)(?:\\:(\\d+))?");
-
-	public static Proxy parseProxy(String proxyDefinition) {
-		if(proxyDefinition == null || proxyDefinition.length() == 0) return Proxy.NO_PROXY;
-		
-		Matcher m = PROXY_DEF_PATTERN.matcher(proxyDefinition);
-		if(m.matches()) {
-			String hostName = m.group(1);
-			int port = m.group(2) == null ? 80 : Integer.parseInt(m.group(2));
-			return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(hostName, port));
-		}
-		else throw new IllegalArgumentException(proxyDefinition);
-	}
-
 	public static String trim(String str, CharSequence trimingCharacters) {
 		char[] tc = asSortedCharArray(trimingCharacters);
 		return trimLeft(trimRight(str, tc), tc); 
