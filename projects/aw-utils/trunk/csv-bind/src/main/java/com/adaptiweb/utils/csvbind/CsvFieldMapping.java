785d88ddd49b232f8e82f45a4e3aa657a1c7176e
miso.moravcik <miso.moravcik@69fc3368-14c8-11de-8b5c-13accb87f508>
2009-11-04 21:27:29 +0000

    csv-bind implementation added

---
+++ projects/aw-utils/trunk/csv-bind/src/main/java/com/adaptiweb/utils/csvbind/CsvFieldMapping.java
@@ -0,0 +1,199 @@
+package com.adaptiweb.utils.csvbind;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+
+import com.adaptiweb.utils.csvbind.editor.CsvFieldPatternEditor;
+
+public class CsvFieldMapping<T> extends ColumnPositionMappingStrategy<T> {
+
+	protected static class CsvFieldDescriptor {
+		private Field field;
+		private CsvField fieldAnnotation;
+		private PropertyEditor fieldEditor;
+		
+		public CsvFieldDescriptor(Field field, CsvField fieldAnnotation) {
+			this.field = field;
+			this.fieldAnnotation = fieldAnnotation;
+		}
+		public Field getField() {
+			return field;
+		}
+		public PropertyEditor getFieldEditor() {
+			return fieldEditor;
+		}
+	}
+	
+	private Map<String, PropertyEditor> fieldEditors = new HashMap<String, PropertyEditor>(); // for efficiency
+	private Set<CsvFieldDescriptor> csvDescriptors = new TreeSet<CsvFieldDescriptor>(
+			new Comparator<CsvFieldDescriptor>() {
+		        public int compare(CsvFieldDescriptor d1, CsvFieldDescriptor d2) {
+		            int i1 = d1.fieldAnnotation.index();
+		            int i2 = d2.fieldAnnotation.index();
+		            return ((Integer) i1).compareTo(i2);
+		        }
+			});
+	
+	protected Integer listIndex = null; 
+	protected ListPropertyEditor listEditor = null;
+
+	/**
+	 * Set target class of bean, include loading input fields and setting editors.
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setType(Class beanClass) {
+		super.setType(beanClass);
+		csvDescriptors.clear();
+		fieldEditors.clear();
+		loadInputFields(beanClass);
+	}
+
+    /**
+     * Retrieve inputFields for {@link ColumnPositionMappingStrategy}.
+     * inputFields array consists of sorted bean fields' names as they appear in CSV file.
+     * Properties in target bean class are annotated by {@link CsvField} annotations.
+     * 
+     * @see ColumnPositionMappingStrategy, CsvField
+     * 
+     * @param beanClass target bean class
+     */
+    private void loadInputFields(Class<T> beanClass) {
+        try {
+        	// find annotated CSV fields
+        	for (Field f : getAllDeclaredFields(beanClass)) {
+        		if (f.isAnnotationPresent(CsvField.class)) {
+        			CsvFieldDescriptor desc = new CsvFieldDescriptor(f, f.getAnnotation(CsvField.class));
+        			desc.fieldEditor = loadInputEditor(f.getName(), desc.fieldAnnotation);
+        			csvDescriptors.add(desc);
+        		}
+        	}
+        } catch (Exception e) {
+			throw new IllegalArgumentException(e);
+        }
+        if (csvDescriptors.isEmpty())
+            throw new IllegalArgumentException("No CSV fields defined for class: " + beanClass.getName());
+
+        List<String> fieldNames = new ArrayList<String>();
+        for (CsvFieldDescriptor desc : csvDescriptors) 
+        	fieldNames.add(desc.field.getName());
+        
+        setColumnMapping(fieldNames.toArray(new String[fieldNames.size()]));
+    }
+
+    /**
+     * Get and store PropertyEditor for field with fieldName by input annotation or default.
+     * @param fieldName
+     * @param annotation
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     * @throws IntrospectionException 
+     */
+    private PropertyEditor loadInputEditor(String fieldName, CsvField fieldAnnotation) throws InstantiationException, IllegalAccessException, IntrospectionException {
+		PropertyEditor editor = null;
+		if (fieldAnnotation.editor() != PropertyEditor.class) { // explicitly defined editor
+			editor = (PropertyEditor) fieldAnnotation.editor().newInstance();
+			if (editor instanceof CsvFieldPatternEditor) 
+				((CsvFieldPatternEditor) editor).setPatterns(fieldAnnotation.patterns());
+			// wrap list editor
+			if (fieldAnnotation.list()) {
+				editor = listEditor = new ListPropertyEditor(editor);
+	            listIndex = fieldAnnotation.index();
+			}
+		} else { // load default editor
+			PropertyDescriptor descriptor = findDescriptor(fieldName);
+			editor = new CsvToBean<T>() {
+				@Override
+				public PropertyEditor getPropertyEditor(PropertyDescriptor propertydescriptor) throws InstantiationException, IllegalAccessException {
+					return super.getPropertyEditor(propertydescriptor);
+				}
+			}.getPropertyEditor(descriptor);
+		}
+		if (editor != null) {
+			fieldEditors.put(fieldName, editor);
+			return editor;
+		} else throw new IllegalArgumentException("No editor for: " + fieldName);
+	}
+
+    /**
+     * Get declared fields of beanClass and all superclasses.
+     * @param beanClass
+     * @return
+     */
+    protected List<Field> getAllDeclaredFields(Class<?> beanClass) {
+    	ArrayList<Field> fields = new ArrayList<Field>();
+    	if (beanClass != null) {
+    		fields.addAll(Arrays.asList(beanClass.getDeclaredFields()));
+    		Object superClass = beanClass.getSuperclass();
+    		if (superClass != null && superClass instanceof Class<?>)
+    			fields.addAll(getAllDeclaredFields((Class<?>) superClass));
+    	}
+    	return fields;
+    }
+
+    /**
+     * Return PropertyEditor for given field.
+     * @param field
+     * @return
+     */
+    public PropertyEditor getEditor(String field) {
+		return fieldEditors.get(field);
+    }
+
+    protected void initMapping() {
+        if (listEditor != null) listEditor.list.clear();
+    }
+    
+    @Override
+    protected String getColumnName(int i) {
+        return super.getColumnName(listIndex == null || i < listIndex ? i : listIndex.intValue());
+    }
+    
+    /**
+     * List CSV editor
+     */
+    public static class ListPropertyEditor extends PropertyEditorSupport {
+        private PropertyEditor itemEditor;
+        private List<Object> list = new ArrayList<Object>();
+        
+        public ListPropertyEditor(PropertyEditor itemEditor) {
+            this.itemEditor = itemEditor;
+        }
+        public void setAsText(String s) throws IllegalArgumentException {
+            itemEditor.setAsText(s);
+            list.add(itemEditor.getValue());
+        }
+        @Override
+        public void setValue(Object value) {
+        	list.clear();
+        	if (value != null && value instanceof List) 
+        		list.addAll((Collection<?>) value);
+        }
+        public Object getValue() {
+            return new ArrayList<Object>(list);
+        }
+		public PropertyEditor getItemEditor() {
+			return itemEditor;
+		}
+    }
+
+	public Collection<CsvFieldDescriptor> getFieldDescriptors() {
+		return csvDescriptors;
+	}
+
+}
---
+++ projects/aw-utils/trunk/csv-bind/src/main/java/com/adaptiweb/utils/csvbind/CsvWriter.java
@@ -0,0 +1,130 @@
+package com.adaptiweb.utils.csvbind;
+
+import java.beans.PropertyEditor;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+import com.adaptiweb.utils.csvbind.CsvFieldMapping.CsvFieldDescriptor;
+import com.adaptiweb.utils.csvbind.CsvFieldMapping.ListPropertyEditor;
+import com.adaptiweb.utils.csvbind.CsvReader.LineAlign;
+
+/**
+ * Wrapped opencsv CSVWriter.
+ * Provides {@link #writeNext(T)} method returning last line as it was written.
+ */
+public class CsvWriter<T> {
+	
+	private class WriterWrapper extends Writer {
+		Writer outputWriter;
+		
+		WriterWrapper(Writer writer) {
+			this.outputWriter = writer;
+		}
+        @Override
+        public void write(String s, int i, int j) throws IOException {
+            outputWriter.write(s, i, j);
+            lastLine.append(s.substring(i, i + j)); // needed to capture last line
+        }
+		@Override
+		public void close() throws IOException {
+			outputWriter.close();
+		}
+		@Override
+		public void flush() throws IOException {
+			outputWriter.flush();
+		}
+		@Override
+		public void write(char[] cbuf, int off, int len) throws IOException {
+			outputWriter.write(cbuf, off, len);
+		}
+	}
+	
+    /**
+     * Bean strategy
+     */
+    protected CsvFieldMapping<T> mapping;
+
+    private CSVWriter csvWriter = null;
+    private StringBuffer lastLine = null;
+    
+    public CsvWriter(Writer writer, Class<T> beanClass, char separator, char quotechar) throws IOException {
+        csvWriter = new CSVWriter(new WriterWrapper(writer), separator, quotechar);
+        mapping = new CsvFieldMapping<T>();
+        mapping.setType(beanClass);
+    }
+
+    public CsvWriter(Writer writer, Class<T> beanClass, LineAlign align) throws IOException {
+    	this(writer, beanClass, CsvConstants.EXCEL_SEPARATOR, CsvConstants.EXCEL_QUOTECHAR);
+    }
+    
+    /**
+     * 
+     * @param output T to write.
+     * @return Returns written line.
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalAccessException 
+     */
+    public String writeNext(T bean) throws CsvException {
+    	mapping.initMapping(); // clear list cache
+    	
+        lastLine = new StringBuffer();
+        try {
+        	List<String> stringCols = new ArrayList<String>();
+	        for (CsvFieldDescriptor desc : mapping.getFieldDescriptors()) {
+	        	Object value = PropertyUtils.getProperty(bean, desc.getField().getName());
+	        	if (value == null) stringCols.add("");
+	        	else {
+		        	PropertyEditor editor = desc.getFieldEditor();
+		        	if (value instanceof Collection  // handle list field
+		        			&& editor instanceof CsvFieldMapping.ListPropertyEditor) {
+		        		
+		        		PropertyEditor itemEditor = ((ListPropertyEditor) editor).getItemEditor();
+		        		for (Object valueItem : (Collection<?>) value) {
+		        			if (valueItem == null) stringCols.add("");
+		        			else {
+		            			itemEditor.setValue(valueItem);
+		            			stringCols.add(itemEditor.getAsText());
+		        			}
+		        		}		        		
+		        	} else { // handle standard fields
+			        	editor.setValue(value);
+			        	stringCols.add(editor.getAsText());
+		        	}
+	        	}
+	        }
+	        csvWriter.writeNext(stringCols.toArray(new String[stringCols.size()]));
+	        
+        } catch (Exception e) {
+        	throw new CsvException("Error writing CSV line", e);
+        }
+        return lastLine.toString();
+    }
+    
+    public String writeComment(String comment) {
+    	lastLine = new StringBuffer();
+    	csvWriter.writeNext(new String[] { CsvConstants.COMMENT_CHAR + comment});
+    	return lastLine.toString();
+    }
+    
+    /**
+     * Close underlying resources.
+     */
+    public void closeResources() {
+        if (csvWriter != null) {
+            try {
+                csvWriter.close();
+            } catch (IOException e) {
+            	e.printStackTrace();
+            }
+        }
+    }
+}
