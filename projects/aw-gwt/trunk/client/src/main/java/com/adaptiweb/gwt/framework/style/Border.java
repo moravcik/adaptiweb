04ff79cb13b59d79ead3e769815280d09132fb8c
miso.moravcik@gmail.com <miso.moravcik@gmail.com@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-03-05 01:46:40 +0000

    added border style

---
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/framework/style/Border.java
@@ -0,0 +1,50 @@
+package com.adaptiweb.gwt.framework.style;
+
+import com.google.gwt.dom.client.Style.BorderStyle;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Element;
+
+public class Border {
+	
+	public static final Style SOLID_SILVER = border(1, Unit.PX, BorderStyle.SOLID, Color.Silver);
+	
+	public static final Style width(final int width, final Unit unit) {
+		return new Style() {
+			@Override
+			public void apply(Element element) {
+				element.getStyle().setBorderWidth(width, unit);
+			}
+		};
+	}
+
+	public static final Style style(final BorderStyle style) {
+		return new Style() {
+			@Override
+			public void apply(Element element) {
+				element.getStyle().setBorderStyle(style);
+			}
+		};
+	}
+
+	public static final Style color(final ColorType color) {
+		return new Style() {
+			@Override
+			public void apply(Element element) {
+				element.getStyle().setBorderColor(color.colorValue());
+			}
+		};
+	}
+	
+	public static final Style border(final int width, final Unit unit, final BorderStyle style, final ColorType color) {
+		return new Style() {
+			@Override
+			public void apply(Element element) {
+				element.getStyle().setBorderWidth(width, unit);
+				element.getStyle().setBorderStyle(style);
+				element.getStyle().setBorderColor(color.colorValue());
+			}
+		};
+	}
+	
+
+}
