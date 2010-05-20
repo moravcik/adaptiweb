0acf0004484257b1f1b183623fbcd864f21a5b61
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-05-20 12:42:05 +0000

    Added support for developing environment.

--- projects/aw-utils/trunk/config-injection/src/main/java/com/adaptiweb/utils/ci/LiveFileHandler.java
+++ projects/aw-utils/trunk/config-injection/src/main/java/com/adaptiweb/utils/ci/LiveFileHandler.java
@@ -1,6 +1,7 @@
 package com.adaptiweb.utils.ci;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +18,8 @@ import com.adaptiweb.utils.commons.VariableResolver;
 
 public class LiveFileHandler implements LiveFile {
 
+	private static final String DEFAULT_CHECK_PERIOD_SYSTEM_PROPERTY = "liveFile_defaultCheckPeriod";
+	private static final String DEVELOPMENT_MODE_SYSTEM_PROPERTY = "liveFile_developmentMode";
 	private static final String CLASSPATH_URL = "classpath:";
 	private VariableResolver variables;
 	private File file;
@@ -25,19 +28,26 @@ public class LiveFileHandler implements LiveFile {
 	private long changesCheckPeriod = 2 * 60 * 1000; //default 2 minutes
 	private long lastChanged;
 	private String templateResource;
+	private boolean developmentMode = false;
 
 	public void setPropertyFile(String fileName) {
 		this.fileName = fileName;
 		this.file = null;
+
+		String changesCheckPeriod = System.getProperty(DEFAULT_CHECK_PERIOD_SYSTEM_PROPERTY);
+		if (changesCheckPeriod != null) setChangesCheckPeriod(Integer.parseInt(changesCheckPeriod));
+
+		String developmentMode = System.getProperty(DEVELOPMENT_MODE_SYSTEM_PROPERTY);
+		if (developmentMode != null) this.developmentMode = Boolean.parseBoolean(developmentMode);
 	}
-	
+
 	/**
 	 * Enables substituting variables in values. Optional.
 	 */
 	public void setVariables(VariableResolver variables) {
 		this.variables = variables;
 	}
-	
+
 	/**
 	 * Minimal change checking period in seconds.
 	 * Optional, default is 2 minutes.
@@ -45,25 +55,29 @@ public class LiveFileHandler implements LiveFile {
 	public void setChangesCheckPeriod(int changesCheckPeriod) {
 		this.changesCheckPeriod = changesCheckPeriod * 1000L;
 	}
-	
+
 	/**
 	 * Enable auto creating missing file during initialization. Optional.
 	 */
 	public void setTemplateResource(String templateResource) {
 		this.templateResource = templateResource;
 	}
-	
+
 	public void checkChanges(FileLoader loader) {
 		long currentTime = System.currentTimeMillis();
 		if (currentTime - lastChecked < changesCheckPeriod) return;
 		lastChecked = currentTime;
 
-		long changedTime = getFile().lastModified();
+		File file = getFile();
+
+		long changedTime = file.lastModified();
 		if (changedTime == lastChanged) return;
+		if (changedTime == 0 && templateResource != null && !file.exists())
+			createDefaultFile(substituteVariables(templateResource), file);
 		lastChanged = changedTime;
-		
+
 		try {
-			loader.loadFile(getFile());
+			loader.loadFile(file);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -72,27 +86,46 @@ public class LiveFileHandler implements LiveFile {
 	private String substituteVariables(String string) {
 		return variables == null ? string : variables.replaceVariables(string);
 	}
-	
+
 	@PostConstruct
 	public File getFile() {
 		if (file == null) {
 			if (fileName == null)
 				throw new IllegalStateException("Attribute 'propertyFile' wasn't initialized!");
-			
+
 			file = new File(substituteVariables(fileName));
-			
+
+			if (developmentMode) prepareDevelopmentMode();
+
 			if (!file.exists() && templateResource != null)
 				createDefaultFile(substituteVariables(templateResource), file);
 		}
 		return file;
 	}
-	
+
+	private void prepareDevelopmentMode() {
+		if (templateResource == null)  return;
+		File templateFile = resourceAsFile();
+		if (templateFile != null && templateFile.exists()) {
+			file = templateFile;
+			templateFile = null;
+		}
+	}
+
+	private File resourceAsFile() {
+		try {
+			return ResourceUtils.getFile(substituteVariables(templateResource));
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+	}
+
 	public static void createDefaultFile(String templateResource, File file) {
 		InputStream is = null;
 		OutputStream os = null;
 		try {
 			FileUtils.forceMkdir(file.getParentFile());
-			
+
 			if (templateResource.startsWith(CLASSPATH_URL)) {
 				templateResource = templateResource.substring(CLASSPATH_URL.length());
 				is = new ClassPathResource(templateResource).getInputStream();
