dc203b9bcea547d2b070b253ec10e58336b71b84
miso.moravcik <miso.moravcik@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-06-03 23:21:13 +0000

    refactored parameter mechanism, generalized Parameter as ParameterExtractor and ParameterBinder\nParameterMap key rolled back to string

--- projects/aw-utils/branches/extended-solargis/commons-utils/src/main/java/com/adaptiweb/utils/commons/param/ParameterQueryParser.java
+++ projects/aw-utils/branches/extended-solargis/commons-utils/src/main/java/com/adaptiweb/utils/commons/param/ParameterQueryParser.java
@@ -1,7 +1,5 @@
 package com.adaptiweb.utils.commons.param;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class ParameterQueryParser {
 
@@ -9,30 +7,22 @@ public class ParameterQueryParser {
 		String decode(String value);
 	}
 
-	public static final class PlainUrlDecoder implements UrlDecoderProvider {
+	public static final UrlDecoderProvider plainUrlDecoder = new UrlDecoderProvider() {
 		@Override
 		public String decode(String value) {
 			return value;
 		}
-	}
+	};
 	
-	public static ParameterMap parseParameters(String queryString, UrlDecoderProvider decoder, Parameter... parameters) {
+	public static ParameterMap parseParameters(String queryString, UrlDecoderProvider decoder) {
 		ParameterMap parameterMap = new ParameterMap();
-		Map<String, Parameter> parameterNames = prepareParameterNames(parameters);
 		String parameterQuery = queryString.startsWith("?") || queryString.startsWith("#") ? queryString.substring(1) : queryString;
 		for (String param : parameterQuery.split("&")) {
 			String[] nameAndValue = param.split("=");
-			if (nameAndValue.length == 2 && parameterNames.containsKey(nameAndValue[0])) {
-				parameterMap.put(parameterNames.get(nameAndValue[0]), decoder.decode(nameAndValue[1]));
-			}
+			if (nameAndValue.length == 2) 
+				parameterMap.put(nameAndValue[0], decoder.decode(nameAndValue[1]));
 		}
 		return parameterMap;
 	}
 
-	private static Map<String, Parameter> prepareParameterNames(Parameter[] parameters) {
-		Map<String, Parameter> parameterNames = new HashMap<String, Parameter>();
-		for (Parameter param : parameters) parameterNames.put(param.getParameterName(), param);
-		return parameterNames;
-	}
-	
 }
