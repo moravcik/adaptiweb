f2d50ef0fa3fafc2ae482b673b517f04848f287d
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-06-22 13:50:22 +0000

    Introduced GwtCodesvrFileter

--- projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/gwt/conf/UriRelatedConfiguration.java
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/gwt/conf/UriRelatedConfiguration.java
@@ -1,6 +1,5 @@
 package com.adaptiweb.gwt.conf;
 
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 import org.springframework.util.AntPathMatcher;
@@ -8,15 +7,15 @@ import org.springframework.util.AntPathMatcher;
 import com.adaptiweb.utils.commons.ArrayUtils;
 import com.adaptiweb.utils.commons.VariableResolver;
 
-class UriRelatedConfiguration {
+public class UriRelatedConfiguration {
 
 	private static final AntPathMatcher pathMatcher = new AntPathMatcher();
 
-	String[] includes;
-	String[] excludes;
-	
+	private String[] includes;
+	private String[] excludes;
+
 	public UriRelatedConfiguration() {}
-	
+
 	public UriRelatedConfiguration(VariableResolver variables, String variableBaseName) {
 		setIncludes(variables.resolveValue(variableBaseName + ".includes", ""));
 		setExcludes(variables.resolveValue(variableBaseName + ".excludes", ""));
@@ -26,31 +25,31 @@ class UriRelatedConfiguration {
 		validatePathPatterns(includes);
 		this.includes = this.includes == null ? includes : ArrayUtils.merge(this.includes, includes);
 	}
-	
+
 	public void setIncludes(String includes) {
-		setIncludeList(toArray(includes));
+		setIncludeList(split(includes));
 	}
-	
+
 	public void setExcludeList(String[] excludes) {
 		validatePathPatterns(excludes);
 		this.excludes = this.excludes == null ? excludes : ArrayUtils.merge(this.excludes, excludes);
 	}
 
 	public void setExcludes(String excludes) {
-		setExcludeList(toArray(excludes));
+		setExcludeList(split(excludes));
 	}
-	
-	private String[] toArray(String includes) {
-		LinkedList<String> collect = new LinkedList<String>();
-		StringTokenizer tokenizer = new StringTokenizer(includes);
-		while (tokenizer.hasMoreTokens()) {
-			String token = tokenizer.nextToken();
-			if (token.length() > 0) collect.add(token);
-		}
-		String[] array = collect.toArray(new String[collect.size()]);
-		return array;
+
+	private static String[] split(String patterns) {
+		StringTokenizer tokenizer = new StringTokenizer(patterns);
+		String[] result = new String[tokenizer.countTokens()];
+		int counter = 0;
+
+		while (tokenizer.hasMoreTokens())
+			result[counter++] = tokenizer.nextToken();
+
+		return result;
 	}
-	
+
 	private static String[] validatePathPatterns(String[] paths) {
 		for (int i = 0; i < paths.length; i++)
 			if (!paths[i].startsWith("/"))
@@ -68,7 +67,7 @@ class UriRelatedConfiguration {
 			if (pathMatcher.match(include, requestUri)) return true;
 		return false;
 	}
-	
+
 	private boolean excludes(String requestUri) {
 		if (excludes != null)
 			for (String exclude : excludes)
