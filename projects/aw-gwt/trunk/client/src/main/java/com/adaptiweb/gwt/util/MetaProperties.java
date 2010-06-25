39e4104c5850c81b968c9c7f2335ba4ad3bff3f7
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-06-25 06:19:15 +0000

    little refactoring of MetaProperties

--- projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/util/MetaProperties.java
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/util/MetaProperties.java
@@ -5,24 +5,23 @@ import java.util.HashMap;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.MetaElement;
 import com.google.gwt.dom.client.NodeList;
 
 public class MetaProperties extends HashMap<String, String> {
-	
+
 	private MetaProperties(String prefix) {
 		GWT.log("Creating MetaProperties with prefix=" + prefix , null);
 		NodeList<Element> nodes = Document.get().getElementsByTagName("META");
 		for (int i = 0; i < nodes.getLength(); i++) {
-			Element metaElement = nodes.getItem(i);
-			String name = metaElement.getAttribute("name");
-			if (name == null || !name.startsWith(prefix)) continue;
-			name = name.substring(prefix.length());
-			put(name, metaElement.getAttribute("content"));
+			MetaElement metaElement = MetaElement.as(nodes.getItem(i));
+			if (!metaElement.getName().startsWith(prefix)) continue;
+			put(metaElement.getName().substring(prefix.length()), metaElement.getContent());
 		}
 	}
-	
+
 	private static final HashMap<String,MetaProperties> instances = new HashMap<String, MetaProperties>();
-	
+
 	public static MetaProperties instance(String prefix) {
 		if (!instances.containsKey(prefix)) instances.put(prefix, new MetaProperties(prefix));
 		return instances.get(prefix);
