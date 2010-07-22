fd4a12c3125694c040bae4caffece7184334b3a0
miso.moravcik <miso.moravcik@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-07-22 11:39:51 +0000

    tagging with interface Model

--- projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/mvc/model/ValueChangeModel.java
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/mvc/model/ValueChangeModel.java
@@ -1,8 +1,9 @@
 package com.adaptiweb.gwt.mvc.model;
 
+import com.adaptiweb.gwt.mvc.Model;
 import com.google.gwt.user.client.ui.HasValue;
 
-public interface ValueChangeModel<T> extends HasValue<T>, HasValueChangeInitHandlers<T> {
+public interface ValueChangeModel<T> extends HasValue<T>, HasValueChangeInitHandlers<T>, Model {
 
 	boolean hasValue();
 	
