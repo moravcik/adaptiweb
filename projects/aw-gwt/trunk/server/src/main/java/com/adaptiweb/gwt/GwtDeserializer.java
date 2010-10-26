f9df1b3cef342e706f846e7ef4321dc177b4de18
miso.moravcik <miso.moravcik@gmail.com>
2010-10-26 16:08:38 +0000

    added gwt serialize (on client) deserialize (on server) support to be used outside RPC calls

---
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/gwt/GwtDeserializer.java
@@ -0,0 +1,19 @@
+package com.adaptiweb.gwt;
+
+import org.springframework.util.ClassUtils;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
+
+public class GwtDeserializer {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T deserialize(Class<T> typeClass, String serialized) throws SerializationException {
+		ServerSerializationStreamReader reader = new ServerSerializationStreamReader(typeClass.getClassLoader(), null);
+		reader.prepareToRead(serialized);
+		Object obj = reader.readObject();
+		if (obj != null && ClassUtils.isAssignableValue(typeClass, obj)) return (T) obj;
+		else throw new SerializationException("Failed deserialization: " + typeClass.getName());
+	}
+	
+}
