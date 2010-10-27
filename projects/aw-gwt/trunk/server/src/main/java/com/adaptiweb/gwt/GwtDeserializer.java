701dffb69abb0f435874d8b1ad0718681ca832c2
miso.moravcik <miso.moravcik@gmail.com>
2010-10-27 17:00:40 +0000

    added decoding support linker and method

--- projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/gwt/GwtDeserializer.java
+++ projects/aw-gwt/trunk/server/src/main/java/com/adaptiweb/gwt/GwtDeserializer.java
@@ -1,5 +1,8 @@
 package com.adaptiweb.gwt;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import org.springframework.util.ClassUtils;
 
 import com.google.gwt.user.client.rpc.SerializationException;
@@ -14,6 +17,28 @@ public class GwtDeserializer {
 		Object obj = reader.readObject();
 		if (obj != null && ClassUtils.isAssignableValue(typeClass, obj)) return (T) obj;
 		else throw new SerializationException("Failed deserialization: " + typeClass.getName());
+	}
+	
+	public static Linker decode(String encodedAndSerialized) {
+		String decodedAndSerialized = encodedAndSerialized;
+		try {
+			decodedAndSerialized = URLDecoder.decode(encodedAndSerialized, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// UTF-8 should be supported
+		}
+		return new Linker(decodedAndSerialized);
+	}
+	
+	public static class Linker {
+		String serialized;
+
+		private Linker(String serialized) {
+			this.serialized = serialized;
+		}
+		
+		public <T> T deserialize(Class<T> typeClass) throws SerializationException {
+			return GwtDeserializer.deserialize(typeClass, serialized);
+		}
 	}
 	
 }
