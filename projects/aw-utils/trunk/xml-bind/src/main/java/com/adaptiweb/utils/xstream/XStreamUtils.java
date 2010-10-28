fbb194279c125ab1ada51c2711297f136e240ad9
miso.moravcik <miso.moravcik@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-10-28 12:15:35 +0000

    added XStream support utils to xml-bind

---
+++ projects/aw-utils/trunk/xml-bind/src/main/java/com/adaptiweb/utils/xstream/XStreamUtils.java
@@ -0,0 +1,41 @@
+package com.adaptiweb.utils.xstream;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.mapper.MapperWrapper;
+
+public class XStreamUtils {
+
+	public static class XStreamIgnoringUnknownFields extends XStream {
+		@Override
+		protected MapperWrapper wrapMapper(MapperWrapper next) {
+			return new MapperWrapper(next) {
+				@SuppressWarnings("rawtypes")
+				@Override
+				public boolean shouldSerializeMember(Class definedIn, String fieldName) {
+					if (definedIn == Object.class) return false;
+					return super.shouldSerializeMember(definedIn, fieldName);
+				}
+			};
+		}
+	}
+	
+	private static XStream xstream;
+	private static Set<Class<?>> processedClasses;
+	
+	public static synchronized XStream getXStream(Class<?> annotatedClass) {
+		if (xstream == null) {
+			xstream = new XStreamIgnoringUnknownFields();
+			xstream.setMode(XStream.XPATH_ABSOLUTE_REFERENCES);
+			processedClasses = new HashSet<Class<?>>();
+		}
+		if (!processedClasses.contains(annotatedClass)) {
+			xstream.processAnnotations(annotatedClass);
+			processedClasses.add(annotatedClass);
+		}
+		return xstream;
+	}
+	
+}
