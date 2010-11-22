0a4f81e5decd10abb6c66df3c775ae5b20ed73f0
milan.skuhra <milan.skuhra@69fc3368-14c8-11de-8b5c-13accb87f508>
2010-11-22 15:16:04 +0000

    Fixed Glass - incorrect glass size while scrolling

--- projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/framework/Glass.java
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/framework/Glass.java
@@ -3,34 +3,42 @@ package com.adaptiweb.gwt.framework;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.ScrollEvent;
+import com.google.gwt.user.client.Window.ScrollHandler;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 
-class Glass extends PopupPanel implements ResizeHandler {
-	
+class Glass extends PopupPanel implements ResizeHandler, ScrollHandler {
+
 	private int showCounter = 0;
 
 	public Glass() {
 		add(new Label(""));
+		resetSize();
+
+		Window.addResizeHandler(this);
+		Window.addWindowScrollHandler(this);
+		setPopupPosition(0, 0);
+		setStyleName("glass");
+	}
+
+	private void resetSize() {
 		setPixelSize(
 				Window.getScrollLeft() + Window.getClientWidth(),
-                Window.getScrollTop() + Window.getClientHeight());
-		
-		Window.addResizeHandler(this);
-        setPopupPosition(0, 0);
-        setStyleName("glass");
+				Window.getScrollTop() + Window.getClientHeight());
 	}
 
 	@Override
-	public void onResize(ResizeEvent event) {
-		setPixelSize(event.getWidth(), event.getHeight());
-	}
-	
+	public void onResize(ResizeEvent event) { resetSize(); }
+
+	@Override
+	public void onWindowScroll(ScrollEvent event) { resetSize(); }
+
 	@Override
 	public void show() {
 		if (showCounter++ == 0) super.show();
 	}
-	
+
 	@Override
 	public void hide() {
 		if (--showCounter == 0) super.hide();
