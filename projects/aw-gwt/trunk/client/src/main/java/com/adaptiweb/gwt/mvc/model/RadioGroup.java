fd26efa80bc50b119da0501a7adbb0c6e3a51fe9
miso.moravcik@gmail.com <miso.moravcik@gmail.com@69fc3368-14c8-11de-8b5c-13accb87f508>
2011-05-26 16:05:28 +0000

    added RadioGroup button accessor method

--- projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/mvc/model/RadioGroup.java
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/mvc/model/RadioGroup.java
@@ -37,6 +37,10 @@ public class RadioGroup<T> extends Listeners<ChangeListener<T>> implements Value
 		rb.addValueChangeHandler(this);
 		return rb;
 	}
+	
+	public RadioButton getRadioButton(T value) {
+		return buttons.get(value);
+	}
 
 	public void onValueChange(ValueChangeEvent<Boolean> event) {
 		if (values.containsKey(event.getSource()) && event.getValue())
