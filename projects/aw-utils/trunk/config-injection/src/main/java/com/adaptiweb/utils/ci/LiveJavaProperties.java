58ed73bede1a8da4b0349f1be331ad3bc267b3d2
miso.moravcik@gmail.com <miso.moravcik@gmail.com@69fc3368-14c8-11de-8b5c-13accb87f508>
2011-06-28 15:40:24 +0000

    overriden propertyNames() method (called from spring property configurer)

--- projects/aw-utils/trunk/config-injection/src/main/java/com/adaptiweb/utils/ci/LiveJavaProperties.java
+++ projects/aw-utils/trunk/config-injection/src/main/java/com/adaptiweb/utils/ci/LiveJavaProperties.java
@@ -2,6 +2,7 @@ package com.adaptiweb.utils.ci;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
 
 
 @SuppressWarnings("serial")
@@ -19,6 +20,12 @@ public class LiveJavaProperties extends VariableProperties implements LiveFile.F
 		return super.getProperty(key);
 	}
 
+	@Override
+	public Enumeration<?> propertyNames() {
+		liveFile.checkChanges(this);
+		return super.propertyNames();
+	}
+	
 	@Override
 	public void loadFile(File file) throws IOException {
 		loadFromFile(file);
