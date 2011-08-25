2705ddb1166d68c68cdf24970b0856c615f98818
miso.moravcik <miso.moravcik@69fc3368-14c8-11de-8b5c-13accb87f508>
2011-08-25 11:15:12 +0000

    fixed nested evaluation of LiveProperties

---
+++ projects/aw-utils/trunk/livefile/src/test/java/com/adaptiweb/utils/livefile/LivePropertiesFactoryBeanTest.java
@@ -0,0 +1,35 @@
+package com.adaptiweb.utils.livefile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class LivePropertiesFactoryBeanTest {
+
+	@Value("#{testProperties.a}")
+	String a;
+	
+	@Value("#{testProperties.b}")
+	String b;
+	
+	@Value("#{testProperties.c}")
+	String c;
+	
+	@Value("#{testProperties.d}")
+	String d;
+	
+	@Test
+	public void test() {
+		assertEquals("a", a);
+		assertEquals("ba", b);
+		assertEquals("cba", c);
+		assertEquals("da|ba|cba", d);
+	}
+	
+}
