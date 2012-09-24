dc334842624046b30cead323e2947c0b59035aac
miso.moravcik@gmail.com <miso.moravcik@gmail.com@69fc3368-14c8-11de-8b5c-13accb87f508>
2012-09-24 12:57:53 +0000

    comment added

--- projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/properties/GwtPropertiesSource.java
+++ projects/aw-gwt/trunk/client/src/main/java/com/adaptiweb/gwt/properties/GwtPropertiesSource.java
@@ -4,4 +4,13 @@ public interface GwtPropertiesSource {
 
 	GwtProperties getMetaProperties();
 	
+	// TODO 
+	// support for direct injecting of Gwt properties values into any class, e.g.
+	// 
+	// @GwtPropertiesValue("link.system_email")
+	// String systemEmail;
+	// 
+	// implemented as:
+	// String systemEmail = GwtProperties.instance().get("link.system_email");
+	
 }
